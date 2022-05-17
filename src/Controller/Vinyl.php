8f388155114befe845edb0fb3d9ba55d7fb65783
Cyril <cyrilbaudoin21@gmail.com>
2022-05-17 10:57:05 +0200

    MAJ

--- src/Controller/Vinyl.php
+++ src/Controller/Vinyl.php
@@ -2,31 +2,42 @@
 
 namespace App\Controller;
 
+use ContainerNmXqQ09\getFragment_Renderer_InlineService;
 use Doctrine\ORM\Mapping\Driver\RepeatableAttributeCollection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class Vinyl
+class Vinyl extends AbstractController
 {
     #[Route('/')]
-    public function test () : Response
+    public function homepage () : Response
     {
-        return new \Symfony\Component\HttpFoundation\Response('Salt');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
     public function display (string $slug =null) : Response
     {
-       if ($slug)
-        {
-            $title = 'Genre : ' .u(str_replace('-', ' ', $slug)) ->title(true) ;
-        }
-        else
-        {
+        if ($slug) {
+            $title = 'Genre : ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
             $title = 'All Genres';
         }
-        return new \Symfony\Component\HttpFoundation\Response( $title);
+        return new \Symfony\Component\HttpFoundation\Response($title);
     }
 
 }
